f1fa29ba02e9d0a2605cea1dfd1de18e6cc3d622
ahmedaljabbari <ahmedaljabbari@outlook.com>
2021-02-23 05:27:25 +0100

    deploy2

--- src/test.js
+++ src/test.js
@@ -4,6 +4,7 @@ function Test(){
     return (
         <div>
             <img src="./images/11.png" alt="jj"></img>
+            <img src="/images/logoP.png" alt="jj"></img>
         </div>
     )
 }
